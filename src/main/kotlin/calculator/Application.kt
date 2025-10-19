ec63fd87747ae03f7729d6d6e7c3505952269ee5
전형진 <gudwls47@naver.com>
2025-10-19 19:48:10 +0900

    feat(calc): 기본 구분자(, :)로 분리해 합계 계산

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,20 @@
 package calculator
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    val input = Console.readLine() ?: ""
+
+    val delimiters: MutableList<Char> = mutableListOf(',', ':')
+
+    val regex = delimiters
+        .map { Regex.escape(it.toString()) }
+        .joinToString("|")
+        .toRegex()
+
+    val sum = input
+        .split(regex)
+        .sumOf { it.toInt() }
+
+    println("결과 : $sum")
 }
